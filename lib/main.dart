6e345842e782c15c40612c652c4b56fee2bb102b
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2026-04-23 20:27:15 -0500

    fix: app orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
+import 'core/platform/app_orientation.dart';
 import 'injection/service_locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AppOrientation.lockPortrait();
   await dotenv.load(fileName: ".env");
   setupLocator();
   runApp(const MyApp());
