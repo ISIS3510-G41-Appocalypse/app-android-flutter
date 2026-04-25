49dadb7644c577d885ca5ad6e9322f28f0bc5702
jpreyesr <jp.reyesr1@uniandes.edu.co>
2026-04-24 23:43:03 -0500

    implementacion de caching strategy en consulta de zonas

--- lib/features/ride_offers/data/data_sources/ride_offers_remote_datasource_impl.dart
+++ lib/features/ride_offers/data/data_sources/ride_offers_remote_datasource_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_plus/dio_cache_plus.dart';
 
 import '../../../../core/errors/error_handler.dart';
 import '../../../../core/errors/exceptions.dart';
@@ -29,15 +30,24 @@ class RideOffersRemoteDataSourceImpl implements RideOffersRemoteDataSource {
     return _getTableRows(
       path: _zonesPath,
       queryParameters: const {'select': 'id,name'},
+      options: Options().setCachingWithDuration(
+        enableCache: true,
+        duration: const Duration(hours: 6),
+      ),
     );
   }
 
   Future<List<Map<String, dynamic>>> _getTableRows({
     required String path,
     required Map<String, dynamic> queryParameters,
+    Options? options,
   }) async {
     try {
-      final response = await dio.get(path, queryParameters: queryParameters);
+      final response = await dio.get(
+        path,
+        queryParameters: queryParameters,
+        options: options,
+      );
 
       final data = response.data;
 
