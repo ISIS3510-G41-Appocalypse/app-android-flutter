16502e15f5cf7e9744122f99134d889df1a6957f
jpreyesr <jp.reyesr1@uniandes.edu.co>
2026-05-21 16:58:51 -0500

    contratos para sistema de calificaciones

---
+++ lib/features/ratings/domain/entities/rate_rider.dart
@@ -0,0 +1,19 @@
+class RateRider {
+  final int riderId;
+  final int driverId;
+  final int punctuality;
+  final int behavior;
+  final int communication;
+  final int paymentPunctuality;
+  final String rideId;
+
+  const RateRider({
+    required this.riderId,
+    required this.driverId,
+    required this.punctuality,
+    required this.behavior,
+    required this.communication,
+    required this.paymentPunctuality,
+    required this.rideId,
+  });
+}
